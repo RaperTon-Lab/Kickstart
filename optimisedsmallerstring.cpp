60b8929e59b7aa07aa7985fd90b98fa232056d82
Ranbir <36307203+RaperTon-Lab@users.noreply.github.com>
2021-07-24 13:14:09 +0530

    Update optimisedsmallerstring.cpp

--- optimisedsmallerstring.cpp
+++ optimisedsmallerstring.cpp
@@ -1,3 +1,33 @@
+/*You are given an integer K and a string S of length N, consisting of lowercase letters from the first K letters of the English alphabet. Find the number of palindrome strings of length N which are lexicographically smaller than S and consist of lowercase letters from the first K letters of the English alphabet.
+
+A string composed of ordered letters a1,a2,…,an is lexicographically smaller than another string of the same length b1,b2,…,bn if ai<bi, where i is the first index where characters differ in the two strings. For example, the following strings are arranged in lexicographically increasing order: aaa, aab, aba, cab.
+
+A palindrome is a string that is the same written forwards and backwards. For example, anna, racecar, aaa and x are all palindromes, while ab, frog and yoyo are not.
+
+As the number of such strings can be very large, print the answer modulo 109+7.
+
+Input
+The first line of the input gives the number of test cases, T. T test cases follow.
+
+Each test case consists of two lines. The first line contains two integers N and K. The second line contains a string S of length N, consisting of lowercase letters.
+
+Output
+For each test case, output one line containing Case #x: y, where x is the test case number (starting from 1) and y is the number of lexicographically smaller palindrome strings modulo 109+7.
+
+Limits
+Memory limit: 1 GB.
+1≤T≤100.
+The string S consists of lowercase letters from the first K letters of the English alphabet.
+Test Set 1
+Time limit: 20 seconds.
+1≤N≤8.
+1≤K≤5.
+Test Set 2
+Time limit: 10 seconds.
+1≤N≤105.
+1≤K≤26.*/
+
+// Takes 0.048s in running test set 2
 #include <bits/stdc++.h>
 
 #define M 1000000007
